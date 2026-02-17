3a885cea0d5f25bef907575734fb81d93b12c5db
JakeWolfe3 <123993306+JakeWolfe3@users.noreply.github.com>
2026-02-17 17:53:23 -0500

    Update Double_motor_testrun_copy.ino

--- autofield/Double_motor_testrun_copy.ino
+++ autofield/Double_motor_testrun_copy.ino
@@ -1,9 +1,12 @@
 const int potpin = A0; //analog pin used to connect the center pin of potentiometer
 
-int pwmpin_l = 3;
-int pwmpin_r = 5;
-int dirpin_l = 2;
-int dirpin_r = 4;
+// connections to arduino
+int pwmpin_backleft = 3;
+int pwmpin_backright = 5;
+int dirpin_backleft = 2;
+int dirpin_backright = 4;
+
+// ADD for 4 motors, set variables for each wheel for pwp and direction
 
 int pwmpin_val;
 
@@ -11,10 +14,16 @@ int pwmpin_val;
 void setup() {
   // put your setup code here, to run once:
 
-  pinMode(pwmpin_l, OUTPUT);
-  pinMode(pwmpin_r, OUTPUT);
-  pinMode(dirpin_l, OUTPUT);
-  pinMode(dirpin_r, OUTPUT);
+ // back wheel pwm and direction outputs
+  pinMode(pwmpin_backleft, OUTPUT);
+  pinMode(pwmpin_backright, OUTPUT);
+
+  pinMode(dirpin_backleft, OUTPUT);
+  pinMode(dirpin_backright, OUTPUT);
+
+ // ADD front wheel pwm and direction outputs
+
+
   Serial.begin(9600);
 
 }
@@ -23,10 +32,16 @@ void loop() {
   // put your main code here, to run repeatedly:
   pwmpin_val = analogRead(potpin);
   pwmpin_val = map(pwmpin_val, 0, 1023, 0, 255);
-  digitalWrite(dirpin_l, HIGH);
-  digitalWrite(dirpin_r, HIGH);
-  analogWrite(pwmpin_l, pwmpin_val);
-  analogWrite(pwmpin_r, pwmpin_val);
+
+//allows wheels to run opposite direction
+  digitalWrite(dirpin_backleft, HIGH); 
+  digitalWrite(dirpin_backright, HIGH); 
+
+  analogWrite(pwmpin_backleft, pwmpin_val);
+  analogWrite(pwmpin_backright, pwmpin_val);
+
+  // ADD for front wheels
+
   Serial.print("Motor speed pwm command: ");
   Serial.print(pwmpin_val);
   Serial.println();
